d9618af7d0ea88a7e7a7f1de4d425f2ae374d6f2
Mohammad Asif Jahish <100868333+AsifJahish@users.noreply.github.com>
2024-11-21 22:44:31 +0500

    Delete mini2Project directory

--- mini2Project/StudentManagementSystem/courses/serializers.py
+++
@@ -1,8 +0,0 @@
-# courses/serializers.py
-from rest_framework import serializers
-from .models import Enrollment
-
-class EnrollmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Enrollment
-        fields = '__all__'
